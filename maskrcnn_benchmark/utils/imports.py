8902e93a8d00797cdf17c87e397c2bcedc9d9ab8
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-02-21 12:23:52 -0500

    fix: imports util bug

--- maskrcnn_benchmark/utils/imports.py
+++ maskrcnn_benchmark/utils/imports.py
@@ -1,12 +1,12 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from importlib import util
+from importlib.util import spec_from_file_location, module_from_spec
 import sys
 
 
 # from https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 def import_file(module_name, file_path, make_importable=False):
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
+    spec = spec_from_file_location(module_name, file_path)
+    module = module_from_spec(spec)
     spec.loader.exec_module(module)
     if make_importable:
         sys.modules[module_name] = module
