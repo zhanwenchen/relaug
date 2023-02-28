c99beeecb898bfac8ed4031e9c34f1e49fd0ca5d
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-02-28 12:16:31 -0500

    use empty string for env var

--- scripts/train_sgcls_local_vctree_baseline.sh
+++ scripts/train_sgcls_local_vctree_baseline.sh
@@ -53,7 +53,7 @@ else
   export USE_GRAFT=False
   export USE_SEMANTIC=False
   export CONFIG_FILE=configs/e2e_relation_X_101_32_8_FPN_1x_vctree.yaml
-  export STRATEGY=''
+  export STRATEGY="''"
   export BOTTOM_K=-1
   export NUM2AUG=-1
   export MAX_BATCHSIZE_AUG=-1
