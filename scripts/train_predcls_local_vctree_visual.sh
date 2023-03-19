735565df4b1f3fc725ebe8d112eda053b49ab998
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-03-19 19:00:04 +0000

    change cuda units for predcls

--- scripts/train_predcls_local_vctree_visual.sh
+++ scripts/train_predcls_local_vctree_visual.sh
@@ -79,7 +79,7 @@ else
   export ALL_EDGES_FPATH=${DATASETS_DIR}/visual_genome/gbnet/all_edges.pkl
 
   # System variables
-  export CUDA_VISIBLE_DEVICES=1,2,3,4
+  export CUDA_VISIBLE_DEVICES=6,7,8,9
   export NUM_GPUS=$(echo ${CUDA_VISIBLE_DEVICES} | tr -cd , | wc -c); ((NUM_GPUS++))
   export BATCH_SIZE=$((${NUM_GPUS} * ${BATCH_SIZE_PER_GPU}))
   export PORT=$(comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1)
