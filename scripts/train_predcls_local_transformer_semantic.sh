67b943eaca70fee4bc1845cf7354a38f5b5d2f6f
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-03-20 12:37:13 -0400

    Use shared functions

--- scripts/train_predcls_local_transformer_semantic.sh
+++ scripts/train_predcls_local_transformer_semantic.sh
@@ -1,46 +1,10 @@
 #!/bin/bash
 
-timestamp() {
-  date +"%Y%m%d%H%M%S"
-}
-
 SLURM_JOB_NAME=transformer_none_semantic_predcls_4GPU_labx_1e3
-SLURM_JOB_ID=$(timestamp)
-
-error_exit()
-{
-#   ----------------------------------------------------------------
-#   Function for exit due to fatal program error
-#       Accepts 1 argument:
-#           string containing descriptive error message
-#   Source: http://linuxcommand.org/lc3_wss0140.php
-#   ----------------------------------------------------------------
-    echo "$(timestamp) ERROR ${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
-    echo "$(timestamp) ERROR ${PROGNAME}: Exiting Early."
-    exit 1
-}
-
-error_check()
-{
-#   ----------------------------------------------------------------
-#   This function simply checks a passed return code and if it is
-#   non-zero it returns 1 (error) to the calling script.  This was
-#   really only created because I needed a method to store STDERR in
-#   other scripts and also check $? but also leave open the ability
-#   to add in other stuff, too.
-#
-#   Accepts 1 arguments:
-#       return code from prior command, usually $?
-#  ----------------------------------------------------------------
-    TO_CHECK=${1:-0}
-
-    if [ "$TO_CHECK" != '0' ]; then
-        return 1
-    fi
-
-}
 
 export PROJECT_DIR=/localtmp/pct4et/relaug
+source ${PROJECT_DIR}/shared_functions/utils.sh
+SLURM_JOB_ID=$(timestamp)
 export MODEL_NAME="${SLURM_JOB_ID}_${SLURM_JOB_NAME}"
 export LOGDIR=${PROJECT_DIR}/log
 MODEL_DIRNAME=${PROJECT_DIR}/checkpoints/${MODEL_NAME}/
