873f404f60ab1ad89c8b018e605d4567e31c9ad9
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-04-03 19:48:12 -0400

    add seed

--- scripts/train_bpl_sa_local.sh
+++ scripts/train_bpl_sa_local.sh
@@ -7,12 +7,12 @@ export CUDA_VISIBLE_DEVICES=1,2,3,4 # TODO: change this.
 
 export MODEL_NAME="${MODEL_NAME_BASE}_${ITERATION}_bpl_sa"
 export PRETRAINED_MODEL_CKPT=${PROJECT_DIR}/checkpoints/${MODEL_NAME_BASE}/model_${ITERATION}.pth
-
 export PROJECT_DIR=/localtmp/pct4et/relaug
 source ${PROJECT_DIR}/scripts/shared_functions/utils.sh
 export LOGDIR=${PROJECT_DIR}/log
 MODEL_DIRNAME=${PROJECT_DIR}/checkpoints/${MODEL_NAME}/
 MODEL_DIRNAME_BASE=${PROJECT_DIR}/checkpoints/${MODEL_NAME_BASE}/
+export SEED=1234
 
 if [ -d "${MODEL_DIRNAME_BASE}" ]; then
   if [ -d "${MODEL_DIRNAME}" ]; then
